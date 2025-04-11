8446b6e16bf1c8c46b36984541e4c6706fad7c3b
mercy chepkemoi koech <koechmercyk@gmail.com>
2025-04-11 23:43:23 +0300

    another

---
+++ src/components/ExpenseForm.jsx
@@ -0,0 +1,1 @@
+//a form to input new expenses
---
+++ src/components/ExpenseTable.jsx
@@ -0,0 +1,24 @@
+//display expenses
+
+const ExpenseTable = ({ expenses, onDelete }) => {
+
+return (
+    <table>
+        <thead>
+            <tr>
+                <th>Description</th>
+                <th>Amount</th>
+                <th>Category</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+
+        <tbody>
+
+        </tbody>
+    </table>
+)
+
+}
+
+export default ExpenseTable;
